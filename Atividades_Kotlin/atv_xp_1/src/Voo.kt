5c0930b02c15a0ba533992b48f4e35439491f02f
Juliano Visoli Melato <julijmfc@gmail.com>
2024-08-24 16:05:06 -0300

    Update Voo.kt

--- Atividades_Kotlin/atv_xp_1/src/Voo.kt
+++ Atividades_Kotlin/atv_xp_1/src/Voo.kt
@@ -2,9 +2,22 @@ class Voo(var numeroDoVoo: Int, var assentosDisponiveis: Array<Int>)
 {
     fun reservarAssento(assento: Int): Boolean
     {
-        // TODO
-        
-        return // true ou false
-    }
+        if (assento > 30 || assento < 1)
+        {
+            println("Esse assento não existe.")
+            return false
+        }
 
+        if (assentosDisponiveis[assento] == 0)
+        {
+            assentosDisponiveis[assento] = 1
+            return true
+        }
+        else
+        {
+            println("Assento indisponível")
+        }
+
+        return false
+    }
 }
